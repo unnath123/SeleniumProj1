38ab93c7e69393a0c5bb098b09b86d5abda9448f
unnath123 <unnathpgowda@gmail.com>
2025-03-05 02:11:15 +0530

    initialsetup

---
+++ src/main/java/com/spiceJetcrm/pages/SignupPage.java
@@ -0,0 +1,5 @@
+package com.spiceJetcrm.pages;
+
+public class SignupPage {
+
+}
---
+++ src/main/java/com/spiceJetcrm/pages/loginPage.java
@@ -0,0 +1,10 @@
+package com.spiceJetcrm.pages;
+
+public class loginPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
